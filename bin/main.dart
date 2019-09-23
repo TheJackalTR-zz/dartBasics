58e8bdd11b778d687b2d4da6e129612033c4b96b
TheJackalTR <aryen.hemvatan@std.yeditepe.edu.tr>
2019-09-23 14:46:22 +0300

    push

--- bin/main.dart
+++ bin/main.dart
@@ -13,7 +13,8 @@ main() {
 }
 */
 
-main() {
+/* 
+  main() {
   int sayi = 10;
   print(sayi);
 
@@ -21,5 +22,92 @@ main() {
   //sayi = 11.1;
 
   sayi = 11;
-  double ondalik = 11;
+  double ondalik = 11.2;
+  double pi = 3.14;
+
+  print(ondalik);
+  print(pi);
+
+  ondalik = 11;
+  print(ondalik);
+
+  var e = 1.42e5;
+  print(e);
+
+}
+ */
+
+/* 
+main() {
+  bool cevap = true;
+  //bool cevaplar = 1;
+
+  print(cevap);
+
+  bool comparison = 5 < 2;
+  print(comparison);
+
+}
+ */
+
+/* 
+//dynamic type her türlü değeri alır;
+main(){
+  var deneme;
+
+  print(deneme);
+
+  var deneme2 = '';
+  print(deneme2);
+
+  var deneme3 = ' ';
+  print(deneme3);
+
+  dynamic dinamik = 10;
+  print(dinamik);
+
+  dinamik = 1.23;
+  print(dinamik);
+
+  dinamik = "hello world";
+  print(dinamik);
+  
+  dinamik = true;
+  print(dinamik);
+
+}
+ */
+
+main(){
+  String mesaj = 'Merhaba';
+
+  print('Mesaj değişkeninin karakter sayısı: ${mesaj.length}');  
+  int sayi = mesaj.length;
+  print(sayi);
+
+
+  print(mesaj[1]);
+  //print(mesaj[7]);
+
+  var yeni = mesaj.replaceAll('aba', 'ata');
+  print(yeni);
+  print(mesaj.replaceAll('Merh', 'mert'));
+
+  print(mesaj.toLowerCase());
+  print(mesaj.toUpperCase());
+
+  print(mesaj.substring(2));
+
+  print('  deneme  '.trim());
+  print('  deneme  '.trimLeft());
+  print('  deneme  '.trimRight());
+
+  num numb = 99.1;
+
+  //gitilen sayıdan küçük olmayan en yakın tam sayı değeri
+  print(numb.ceil());
+  //gitilen sayıdan büyük olmayan en yakın tam sayı değeri
+  print(numb.floor());
+
+
 }
